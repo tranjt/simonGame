33a8b6896feb01acac612b1fc693f79da0197f5d
JTTran <tranjt80@gmail.com>
2017-05-23 18:39:33 +0300

    comments added

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,6 @@ const startButton = document.querySelector("button#startButton");
 const strictLightButton = document.querySelector("button#strictButton");
 const onOffButton = document.querySelector("button#onOffButton");
 const gameCounter = document.querySelector("h1#gameCounter");
-
 const gameAnnouncement = document.querySelector("div#gameAnnouncement");
 const gameAnnouncementText = document.querySelector("div#gameAnnouncementText");
 const okButton = document.querySelector("button#okButton");
@@ -43,35 +42,33 @@ const events = {
   }
 };
 
-
+//Handle onoff button events, Inform the simongame onOffButton have been clicked
 function gameOnOff() {	
-	events.emit("TurnOnOff","");
+	events.emit("TurnOnOff", "");
 }
-
+//Handle startButton/okButton events, Inform the simongame startButton/okButton have been clicked
 function gameStart() {	
 	events.emit("StartGame", "");
 }
-
+//Handle user lightButton click event, Inform the simongame a lightButton with id have been clicked
 function makeMove(event){
 	events.emit("PlayerHasMoved", event.target.className.split(" ")[1]);
 }
-
+//Handle strictLightButton event, Inform the simongame strictLightButton have been clicked
 function strictOnOff() {	
 	events.emit("StrictOnOff", strictLightButton);
 }
 
-// //Ok button, Restart game once game is over
-// function confirm(event){
-// 	events.emit("StartGame", "");
-// }
 /*
 	Game Board
 */
-const GameBoard = function (buttons = lightButtons, sounds = audioFiles) { 
+
+const GameBoard = function (buttons, sounds) { 
 	this.buttons = buttons;
 	this.sounds = sounds;	
 }
-
+//Light Button blink by changing to a lighter/darker background color with css class
+//then back to orginal after preset time 550 ms.
 GameBoard.prototype.buttonBlink = function(index, cssClass) {	
 	this.buttons[index].classList.add(cssClass);
 	setTimeout(() => {
@@ -80,6 +77,7 @@ GameBoard.prototype.buttonBlink = function(index, cssClass) {
 }
 
 GameBoard.prototype.playsound = function(index) {	
+	this.sounds[index].currentTime = 0;
 	this.sounds[index].play();	
 }
 
@@ -91,12 +89,15 @@ const GameOption = function() {
 	this.onoffState = false;	
 	this.gameCounter = 0;
 }
-
+//Handle on/off button on game option panel
 GameOption.prototype.setOnOff = function() {
+	//toggle button on/off
 	this.gameOption.onoffState = this.gameOption.onoffState ? false : true;
+	//If is On-state set game counter to display "--"
 	if (this.gameOption.onoffState) {
-		this.gameOption.gameCounter = 0;
+		this.gameOption.gameCounter = "--";
 	}
+	//If Off-state reset all simon game states
 	else {
 		this.gameOption.gameCounter = "";		
 		this.playedList = [];
@@ -104,21 +105,26 @@ GameOption.prototype.setOnOff = function() {
 		this.playerTurn = false;
 		this.gameOption.strict = false;
 		strictLightButton.classList.remove("strict");	
+		startButton.classList.remove("start");
 	}
+	//update changed game counter value in html
 	this.gameOption.updateGameCounter(gameCounter);	
 }
 
+//display game counter value in html
 GameOption.prototype.updateGameCounter = function(htmlgameCounter) {
 	htmlgameCounter.innerHTML = this.gameCounter;
 }
 
+//display game counter as error state "!!!!!" for 600ms then change back to normal value
 GameOption.prototype.displayeError = function(htmlgameCounter) {	
-	htmlgameCounter.innerHTML = "!!!";
+	htmlgameCounter.innerHTML = "!!!!!";
 	setTimeout(() => {
 		htmlgameCounter.innerHTML = this.gameCounter;
-	}, 550);
+	}, 600);
 }
 
+//Set strict value true/false, if true change font color to red on strict button to reflect active
 GameOption.prototype.setStrict = function(strictLightButton) { 
 	if (this.gameOption.onoffState) {
 		this.gameOption.strict  = this.gameOption.strict ? false : true;
@@ -138,73 +144,95 @@ const SimonGame = function (gameBoard, gameOption) {
 	this.playerTurn = false;
 }
 
-SimonGame.prototype.init = function() { //hook up listener	
+//hook up listener	
+SimonGame.prototype.init = function() { 
 	events.on("PlayerHasMoved", this.handlePlayerClick.bind(this)); 	
 	events.on("StartGame", this.reStart.bind(this)); 
 	events.on("TurnOnOff", this.gameOption.setOnOff.bind(this));	
 	events.on("StrictOnOff", this.gameOption.setStrict.bind(this));	
-	//events.on("StartGame", this.reStart.bind(this));
 }
 
 SimonGame.prototype.reStart = function() { 
+	//check if game is on
 	if (this.gameOption.onoffState) {
+		//hide gameAnnouncement/wrapper case restart via okButton
 		gameAnnouncement.classList.add('hidden');
 		wrapper.classList.add('hidden');
+		// reset all simon game states and display game counter
 		this.playedList = [];
 		this.currentSelect = 0;
 		this.playerTurn = false;
 		this.gameOption.gameCounter = 0;		
 		this.gameOption.updateGameCounter(gameCounter);	
-		this.doNextMove();
+		startButton.classList.add("start");
+		this.doNextMove(true);
 	}
 }
 
 SimonGame.prototype.handlePlayerClick = function(index) {
+	//make sure it's players turn ie simongame is not currently playing sound sequense
+	//and the game is on
 	 if (this.playerTurn && this.gameOption.onoffState) {
-	 	this.playerTurn = false;		
-	 	let isValidMove = this.validateMove(index);
-	 	if (isValidMove) {
-	 		this.gameBoard.buttonBlink(index, "light");
-			this.gameBoard.playsound(index);
-	 	}
-	 	else {
-			this.gameOption.displayeError(gameCounter);
-	 		this.gameBoard.buttonBlink(index, "dark");	 
-			this.gameBoard.playsound(index);
-	 	}
-
+	 	//lock out player after click 	 	
+	 	this.playerTurn = false;
+	 	//check if corrent light button clicked 		
+	 	let isValidMove = this.validateMove(index);	
+	 	//display right/wrong button clicked
+	 	this.buttonLightResponse(isValidMove, index);
+	 	//make sure 600ms have past before continue
+	 	//to ensure the sound and blink can complete
 		setTimeout(() => {				
 			if (isValidMove) {
-				this.currentSelect++;	 				
-				if (this.currentSelect === this.playedList.length ) {
-
-					if(this.gameOption.gameCounter === 20) {
-						gameAnnouncementText.innerHTML = "Congratulations you have won!";
-						wrapper.classList.remove('hidden');
-						gameAnnouncement.classList.remove('hidden');
+				this.currentSelect++;
+				//check if player got the whole current sound sequense correct								 				
+				if (this.currentSelect === this.playedList.length) {
+					//check if game is over, game is over if player get 20 correct in a row
+					if (this.gameOption.gameCounter === 20) {
+						this.gameOver();
 						return;
 					}
-					this.currentSelect = 0;					
-					this.doNextMove();
+					//if not over get next playsequense				
+					this.doNextMove(isValidMove);
 					return;
 				}
-				this.playerTurn = true;				
+				this.playerTurn = true; //player can click light buttons again after 600ms				
 			}
-			else {	
+			else {
+				//if player got a wrong light button state and game is in strict mode restart game	
 				if (this.gameOption.strict) {
 					this.reStart();
 					return;
-				}	
-				else {
-					this.currentSelect = 0;
-					this.playSequence(this.playedList);
+				}
+				//if player got a wrong light button state and replay sound sequense from the start
+				else {			
+					this.doNextMove(isValidMove);
 				}	
 			}			 
-		}, 700);
+		}, 600);
 	}	
 }
 
+//display right/wrong button clicked
+SimonGame.prototype.buttonLightResponse = function(isValidMove, lightButtonIndex) { 	
+		if (isValidMove) {
+	 		this.gameBoard.buttonBlink(lightButtonIndex, "light");
+			this.gameBoard.playsound(lightButtonIndex);
+	 	}
+	 	else {
+			this.gameOption.displayeError(gameCounter);
+	 		this.gameBoard.buttonBlink(lightButtonIndex, "dark");	 
+			this.gameBoard.playsound(lightButtonIndex);
+	 	}
+}
 
+//handle game over display a win msg
+SimonGame.prototype.gameOver = function() { 	
+		gameAnnouncementText.innerHTML = "Congratulations you have won!";
+		wrapper.classList.remove('hidden');
+		gameAnnouncement.classList.remove('hidden');
+}
+
+//make sure light buttons is clicked in correct sequense
 SimonGame.prototype.validateMove = function(index) { 	
 	if (this.playedList[this.currentSelect] === parseInt(index) ) {			
 		return true;
@@ -212,32 +240,41 @@ SimonGame.prototype.validateMove = function(index) {
 	return false;
 }
 
+//play sound/light sequense
 SimonGame.prototype.playSequence = function(stepList) { 
+	//make sure player can't click while playing the sequense
 	this.playerTurn = false;
 	for (let i = 0; i < stepList.length; i++) {
+		//make sure each sound/light is played 600ms apart
 		setTimeout(() => {			
 			this.gameBoard.buttonBlink(stepList[i], "light");
 			this.gameBoard.playsound(stepList[i]);
+			//enable player click after sequense is done
 			if (i === (stepList.length-1)) this.playerTurn = true;
-		}, 700 + 700*i);
+		}, 600*i);
 	}
 }
 
-SimonGame.prototype.doNextMove = function() { 	
-	this.playedList.push(Math.floor(Math.random()*4));
-	this.gameOption.gameCounter++;
-	
-	setTimeout(() => {
-		this.playSequence(this.playedList);		
+//handles generation of new state to sound/light sequense and play the sequense
+//doNext value dictates if new state should be made and added to current sound/light sequense or not
+SimonGame.prototype.doNextMove = function(doNext) { 
+	this.currentSelect = 0;		
+	if (doNext) {
+		this.playedList.push(Math.floor(Math.random()*4));
+		this.gameOption.gameCounter++;
 		this.gameOption.updateGameCounter(gameCounter);
-	}, 1000);			
+	}
+	//delay 1500ms before playing the current sound/light sequense 
+	setTimeout(() => {		
+		this.playSequence(this.playedList);				
+	}, 1500);				
 }
 
-const newgameBoard = new GameBoard();
+const newgameBoard = new GameBoard(lightButtons, audioFiles);
 const newgameOption = new GameOption();
 const newgame = new SimonGame(newgameBoard, newgameOption); 
 newgame.init();
 
 
-//User Story: I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
-//do a wrapper display msg on top when 20 reached, ok button restart game 
+
+
